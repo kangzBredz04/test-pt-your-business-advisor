5d61c57f469b1c3c1cf6980353efcc83147c631e
kangzBredz04 <wabredz1234@gmail.com>
2024-09-21 22:04:30 +0700

    Update model Respponse

--- src/main/java/com/yourbussinesadvisor/project/model/Response.java
+++ src/main/java/com/yourbussinesadvisor/project/model/Response.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,19 +19,34 @@ public class Response {
     @JoinColumn(name = "form_id", nullable = false)
     private Form form;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne // Hubungkan dengan model User
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // User yang sedang login
 
     @Column(name = "date", nullable = false)
     private LocalDateTime date;
 
-    // Constructors, getters, setters
+    @OneToMany(mappedBy = "response", cascade = CascadeType.ALL) // Tambahkan relasi ke Answer
+    private List<Answer> answers;
+
+    // Constructors
     public Response() {
+        // Default constructor
     }
 
-    public Response(Form form, Long userId, LocalDateTime date) {
+    // Constructor dengan parameter form, user, dan date
+    public Response(Form form, User user) {
         this.form = form;
-        this.userId = userId;
-        this.date = date;
+        this.user = user; // Menggunakan objek User
+        this.date = LocalDateTime.now(); // Set tanggal saat ini
+    }
+
+    // Metode untuk membuat Response baru dengan tanggal saat ini
+    public static Response createResponse(Form form, User user) {
+        Response response = new Response();
+        response.setForm(form);
+        response.setUser(user);
+        response.setDate(LocalDateTime.now());
+        return response;
     }
 }
