8d5257051c85b4c9c19519c3ba25d5517d6188df
kangzBredz04 <wabredz1234@gmail.com>
2024-09-21 19:44:10 +0700

    tambah file ResponseRepository

---
+++ src/main/java/com/yourbussinesadvisor/project/repository/ResponseRepository.java
@@ -0,0 +1,14 @@
+package com.yourbussinesadvisor.project.repository;
+
+import com.yourbussinesadvisor.project.model.Response;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ResponseRepository extends JpaRepository<Response, Long> {
+    List<Response> findByFormId(Long formId);
+
+    List<Response> findByUserId(Long userId);
+}
