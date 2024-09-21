bd092d95a9557e2436a594da093e254b3d7cb232
kangzBredz04 <wabredz1234@gmail.com>
2024-09-21 22:04:52 +0700

    Update ResponseRepository

--- src/main/java/com/yourbussinesadvisor/project/repository/ResponseRepository.java
+++ src/main/java/com/yourbussinesadvisor/project/repository/ResponseRepository.java
@@ -1,5 +1,6 @@
 package com.yourbussinesadvisor.project.repository;
 
+import com.yourbussinesadvisor.project.model.Form;
 import com.yourbussinesadvisor.project.model.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ public interface ResponseRepository extends JpaRepository<Response, Long> {
     List<Response> findByFormId(Long formId);
 
     List<Response> findByUserId(Long userId);
+
+    List<Response> findByForm(Form form);
 }
