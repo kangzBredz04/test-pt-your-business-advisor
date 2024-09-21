155b43fdb7ade422ee96cfb90eb77b97ebed6651
kangzBredz04 <wabredz1234@gmail.com>
2024-09-21 17:38:27 +0700

    update file QuestionRepositry

--- src/main/java/com/yourbussinesadvisor/project/repository/QuestionRepository.java
+++ src/main/java/com/yourbussinesadvisor/project/repository/QuestionRepository.java
@@ -1,10 +1,13 @@
 package com.yourbussinesadvisor.project.repository;
 
+import com.yourbussinesadvisor.project.model.Form;
 import com.yourbussinesadvisor.project.model.Question;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    // Add any custom query methods if needed
+    Optional<Question> findByIdAndForm(Long id, Form form);
 }
