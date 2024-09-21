2748c739a05c0c8a876c3da3a2033ad9f6cae11c
kangzBredz04 <wabredz1234@gmail.com>
2024-09-21 17:16:07 +0700

    Tambah file QuestionRequest

---
+++ src/main/java/com/yourbussinesadvisor/project/controller/QuestionRequest.java
@@ -0,0 +1,15 @@
+package com.yourbussinesadvisor.project.controller;
+
+import lombok.Data;
+
+import java.util.List;
+
+import com.yourbussinesadvisor.project.model.ChoiceType;
+
+@Data
+public class QuestionRequest {
+    private String name;
+    private ChoiceType choiceType;
+    private List<String> choices; // can be null for types that do not require choices
+    private Boolean isRequired;
+}
